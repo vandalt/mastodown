20e982af238a50b33d3d54aaabfc5c968efaee1c
Thomas Vandal <thomas.vandal@umontreal.ca>
2026-08-26 12:23:14 -0400

    Import some functions directly in __init__.py

--- src/mastodown/__init__.py
+++ src/mastodown/__init__.py
@@ -1,1 +1,5 @@
-from ._version import __version__  # noqa: F401
+from mastodown._version import __version__ as __version__
+from mastodown.download import download_product as download_product
+from mastodown.download import download_products as download_products
+from mastodown.query import get_meta as get_meta
+from mastodown.query import query_obs as query_obs
